0598f997e16c80914856b89c2132e166cb5b744e
quochuywork <huypro0320092003@gmail.com>
2024-03-14 13:37:30 +0700

    add admin reject/approve vendor

--- lib/views/screens/side_bar_screen/widgets/vendor_widget.dart
+++ lib/views/screens/side_bar_screen/widgets/vendor_widget.dart
@@ -1,11 +1,105 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class VendorWidget extends StatelessWidget {
+class VendorWidget extends StatefulWidget {
   const VendorWidget({super.key});
 
   @override
+  State<VendorWidget> createState() => _VendorWidgetState();
+}
+
+class _VendorWidgetState extends State<VendorWidget> {
+  final Stream<QuerySnapshot> _vendorsStream =
+      FirebaseFirestore.instance.collection('vendors').snapshots();
+
+  @override
   Widget build(BuildContext context) {
-    return Row(
+    return StreamBuilder<QuerySnapshot>(
+      stream: _vendorsStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading");
+        }
+
+        return ListView.builder(
+          shrinkWrap: true,
+          itemCount: snapshot.data?.docs.length,
+          itemBuilder: (context, index) {
+            final userData = snapshot.data?.docs[index];
+            final FirebaseFirestore store = FirebaseFirestore.instance;
+            return Row(
+              children: [
+                vendorData(
+                  1,
+                  SizedBox(
+                    height: 50,
+                    width: 50,
+                    child: Image.network(userData?['storeImage']),
+                  ),
+                ),
+                vendorData(
+                  3,
+                  Text(
+                    userData?['businessName'],
+                  ),
+                ),
+                vendorData(
+                  2,
+                  Text(
+                    userData?['cityValue'],
+                  ),
+                ),
+                vendorData(
+                  2,
+                  Text(
+                    userData?['stateValue'],
+                  ),
+                ),
+                vendorData(
+                  1,
+                  userData?['approved'] == true
+                      ? ElevatedButton(
+                          onPressed: () async {
+                            await store
+                                .collection("vendors")
+                                .doc(userData?['vendorId'])
+                                .update({
+                              'approved': false,
+                            });
+                          },
+                          child: const Text("Reject"),
+                        )
+                      : ElevatedButton(
+                          onPressed: () async {
+                            await store
+                                .collection("vendors")
+                                .doc(userData?['vendorId'])
+                                .update({
+                              'approved': true,
+                            });
+                          },
+                          child: const Text("Approve"),
+                        ),
+                ),
+                vendorData(
+                  1,
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text("View more"),
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+
+    Row(
       children: [
         vendorData(
           1,
@@ -15,22 +109,11 @@ class VendorWidget extends StatelessWidget {
             child: Image.network(""),
           ),
         ),
+        vendorData(3, const Text("Huy Store ")),
+        vendorData(2, const Text("City ")),
+        vendorData(1, ElevatedButton(onPressed: () {}, child: Text("Reject"))),
         vendorData(
-          3,
-          const Text("Huy Store ")
-        ),
-        vendorData(
-            2,
-            const Text("City ")
-        ),
-        vendorData(
-            1,
-            ElevatedButton(onPressed: (){}, child: Text("Reject"))
-        ),
-        vendorData(
-            1,
-            ElevatedButton(onPressed: (){}, child: Text("View more"))
-        ),
+            1, ElevatedButton(onPressed: () {}, child: Text("View more"))),
       ],
     );
   }
